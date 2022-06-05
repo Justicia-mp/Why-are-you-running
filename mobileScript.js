60215699cd25e5ef863bf495a962c93b2ebb6fbb
Justicia-mp <95725780+Justicia-mp@users.noreply.github.com>
2022-06-05 19:29:35 +0100

    Update mobileScript.js

--- mobileScript.js
+++ mobileScript.js
@@ -9,7 +9,7 @@ import { updateMoney, setupMoney, getMoneyRects } from "./money.js"
 
 
 const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 60
+const WORLD_HEIGHT = 35
 const SPEED_SCALE_INCREASE = 0.00001
 
 const worldElem = document.querySelector("[data-world]")
